173c201dad67bcea1b7b9069c95da20490e0380c
atrn0 <14242642+aratasato@users.noreply.github.com>
2020-10-15 23:23:18 +0900

    update new.sh

--- new.sh
+++ new.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo New article
-git checkout master
-read -p 'slug: ' slug
-git checkout -b p/$slug
-hugo new posts/$slug/index.md
-open content/posts/$slug/index.md
+git checkout master &&
+read -p 'slug: ' slug &&
+git checkout -b p/$slug &&
+hugo new posts/$slug/index.md &&
+code content/posts/$slug/index.md &&
 echo New article template created
